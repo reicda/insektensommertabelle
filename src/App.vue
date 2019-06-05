0fb238d6b63b73030e64cf16e213010856e632ca
Bjoern Schilberg <bjoern@intevation.de>
2019-06-05 14:34:35 +0200

    Calculations simplified and duplications largely avoided.

--- src/App.vue
+++ src/App.vue
@@ -93,9 +93,9 @@
 
           <template slot="footer" :items="footer" v-if="footer">
             <td colspan="100%">
-              Anzahl Meldungen:
+              Anzahl Meldungen (insgesamt):
               <strong>{{footer.meldungen}}</strong>
-              <br>Anzahl Beobachtungen:
+              <br>Anzahl Beobachtungen (ingesamt):
               <strong>{{footer.beobachtungen}}</strong>
             </td>
           </template>
@@ -109,99 +109,48 @@
 import Chart from "./components/Chart";
 import Papa from "papaparse";
 
-function anzahlMeldungen(insekten) {
-  var beobachtungen = [];
-  insekten.forEach(function(insekt) {
-    beobachtungen.push([insekt.lat, insekt.lon]);
+function anzahlMeldungen(beobachtungen) {
+  let latlon = [];
+  beobachtungen.forEach(function(beobachtung) {
+    latlon.push([beobachtung.lat, beobachtung.lon]);
   });
-
-  var meldungen = beobachtungen
+  // prevent duplicates
+  var meldungen = latlon
     .map(JSON.stringify)
     .reverse()
-    .filter(function(e, i, a) {
-      return a.indexOf(e, i + 1) === -1;
+    .filter(function(value, index, array) {
+      return array.indexOf(value, index + 1) === -1;
     })
     .reverse()
     .map(JSON.parse);
   return meldungen.length;
 }
 
-function anzahlLebensraum(insekten, lebensraum) {
-  let beobachtungen = insekten.filter(
-    insekt => insekt.lebensraum === lebensraum
-  );
-
-  return {
-    beobachtungen: beobachtungen.length,
-    meldungen: anzahlMeldungen(beobachtungen)
-  };
-}
-
-function anzahlBundesland(insekten, bundesland) {
-  let beobachtungen = insekten.filter(
-    insekt => insekt.bundesland === bundesland
-  );
-
-  return {
-    beobachtungen: beobachtungen.length,
-    meldungen: anzahlMeldungen(beobachtungen)
-  };
-}
-
-function top5bundesland(insekten, bundesland) {
-  const bl = insekten.filter(insekt => insekt.bundesland === bundesland);
-  const map = new Map();
-  bl.forEach(item => {
-    const entry = map.get(item.artname);
+function top5(beobachtungen) {
+  // Totaling anzahl and meldungen for unique artname
+  const uniqueMap = new Map();
+  beobachtungen.forEach(beobachtung => {
+    const entry = uniqueMap.get(beobachtung.artname);
     if (!entry) {
-      map.set(item.artname, {
-        artname: item.artname,
-        taxon: item.taxon,
-        gattung: item.gattung,
-        anzahl: Number.parseInt(item.anzahl),
+      uniqueMap.set(beobachtung.artname, {
+        artname: beobachtung.artname,
+        taxon: beobachtung.taxon,
+        gattung: beobachtung.gattung,
+        anzahl: Number.parseInt(beobachtung.anzahl),
         meldungen: 1
       });
     } else {
       ++entry.meldungen;
-      entry.anzahl += Number.parseInt(item.anzahl);
+      entry.anzahl += Number.parseInt(beobachtung.anzahl);
     }
   });
-  const top = [...map.values()];
+  // Create list
+  const top = [...uniqueMap.values()];
+  // Sort by meldungen and then by anzahl
   top.sort(function(a, b) {
-    return b.meldungen - a.meldungen;
+    return b.meldungen - a.meldungen || b.anzahl - a.anzahl;
   });
-  let top5 = top.slice(0, 5);
-  for (let i = 0; i < top5.length; i++) {
-    top5[i].rang = i + 1;
-    top5[i].durchschnitt = Number.parseFloat(
-      Number.parseInt(top5[i].anzahl) / Number.parseInt(top5[i].meldungen)
-    ).toFixed(2);
-  }
-  return top5;
-}
-
-function lebensraumTop5(insekten, lebensraum) {
-  const l = insekten.filter(insekt => insekt.lebensraum === lebensraum);
-  const map = new Map();
-  l.forEach(item => {
-    const entry = map.get(item.artname);
-    if (!entry) {
-      map.set(item.artname, {
-        artname: item.artname,
-        taxon: item.taxon,
-        gattung: item.gattung,
-        anzahl: Number.parseInt(item.anzahl),
-        meldungen: 1
-      });
-    } else {
-      ++entry.meldungen;
-      entry.anzahl += Number.parseInt(item.anzahl);
-    }
-  });
-  const top = [...map.values()];
-  top.sort(function(a, b) {
-    return b.meldungen - a.meldungen;
-  });
+  // Return a top5 with pasted rang into it.
   let top5 = top.slice(0, 5);
   for (let i = 0; i < top5.length; i++) {
     top5[i].rang = i + 1;
@@ -221,7 +170,10 @@ export default {
     return {
       search: "",
       loading: true,
-      footer: {},
+      footer: {
+        beobachtungen: 0,
+        meldungen: 0
+      },
       campains: [
         { text: "Juni 2019", value: "2019-06" },
         { text: "August 2018", value: "2018-08" },
@@ -236,9 +188,8 @@ export default {
         { text: "Gattung", value: "gattung" },
         { text: "Taxon", value: "taxon" }
       ],
-      insects: [],
+      beobachtungen: [],
       tableData: [],
-      top100: [],
       selectedRanking: { name: "Top 100" },
       selectedCampain: { text: "Juni 2019", value: "2019-06" },
       rankings: [
@@ -246,32 +197,72 @@ export default {
         { name: "Top 100", avatar: "images/brd.svg" },
         { divider: true },
         { header: "TOP5 Bundesländer" },
-        { name: "Baden-Württemberg", avatar: "images/bw.svg", group: "TOP5 Bundesländer"},
-        { name: "Bayern", avatar: "images/by.svg", group: "TOP5 Bundesländer"},
-        { name: "Brandenburg & Berlin", avatar: "images/bb_be.svg", group: "TOP5 Bundesländer"},
-        { name: "Hessen", avatar: "images/he.svg", group: "TOP5 Bundesländer"},
-        { name: "Mecklenburg-Vorpommern", avatar: "images/mv.svg", group: "TOP5 Bundesländer"},
-        { name: "Niedersachsen & Bremen", avatar: "images/ni_hb.svg", group: "TOP5 Bundesländer"},
-        { name: "Nordrhein-Westfalen", avatar: "images/nw.svg", group: "TOP5 Bundesländer"},
-        { name: "Rheinland-Pfalz", avatar: "images/rp.svg", group: "TOP5 Bundesländer"},
-        { name: "Saarland", avatar: "images/sl.svg", group: "TOP5 Bundesländer"},
-        { name: "Sachsen", avatar: "images/sn.svg", group: "TOP5 Bundesländer"},
-        { name: "Sachsen-Anhalt", avatar: "images/st.svg", group: "TOP5 Bundesländer"},
+        {
+          name: "Baden-Württemberg",
+          avatar: "images/bw.svg",
+          group: "TOP5 Bundesländer"
+        },
+        { name: "Bayern", avatar: "images/by.svg", group: "TOP5 Bundesländer" },
+        {
+          name: "Brandenburg & Berlin",
+          avatar: "images/bb_be.svg",
+          group: "TOP5 Bundesländer"
+        },
+        { name: "Hessen", avatar: "images/he.svg", group: "TOP5 Bundesländer" },
+        {
+          name: "Mecklenburg-Vorpommern",
+          avatar: "images/mv.svg",
+          group: "TOP5 Bundesländer"
+        },
+        {
+          name: "Niedersachsen & Bremen",
+          avatar: "images/ni_hb.svg",
+          group: "TOP5 Bundesländer"
+        },
+        {
+          name: "Nordrhein-Westfalen",
+          avatar: "images/nw.svg",
+          group: "TOP5 Bundesländer"
+        },
+        {
+          name: "Rheinland-Pfalz",
+          avatar: "images/rp.svg",
+          group: "TOP5 Bundesländer"
+        },
+        {
+          name: "Saarland",
+          avatar: "images/sl.svg",
+          group: "TOP5 Bundesländer"
+        },
+        {
+          name: "Sachsen",
+          avatar: "images/sn.svg",
+          group: "TOP5 Bundesländer"
+        },
+        {
+          name: "Sachsen-Anhalt",
+          avatar: "images/st.svg",
+          group: "TOP5 Bundesländer"
+        },
         {
           name: "Schleswig-Holstein & Hamburg",
           avatar: "images/sh_hh.svg",
           group: "TOP5 Bundesländer"
         },
-        { name: "Thüringen", avatar: "images/th.svg", group: "TOP5 Bundesländer"},
+        {
+          name: "Thüringen",
+          avatar: "images/th.svg",
+          group: "TOP5 Bundesländer"
+        },
         { divider: true },
         { header: "TOP5 Lebensräume" },
-        { name: "Garten", group: "TOP5 Lebensräume"},
-        { name: "Balkon", group: "TOP5 Lebensräume"},
-        { name: "Park", group: "TOP5 Lebensräume"},
-        { name: "Wiese", group: "TOP5 Lebensräume"},
-        { name: "Wald", group: "TOP5 Lebensräume"},
-        { name: "Feld", group: "TOP5 Lebensräume"},
-        { name: "Teich", group: "TOP5 Lebensräume"},
+        { name: "Garten", group: "TOP5 Lebensräume" },
+        { name: "Balkon", group: "TOP5 Lebensräume" },
+        { name: "Park", group: "TOP5 Lebensräume" },
+        { name: "Wiese", group: "TOP5 Lebensräume" },
+        { name: "Wald", group: "TOP5 Lebensräume" },
+        { name: "Feld", group: "TOP5 Lebensräume" },
+        { name: "Teich", group: "TOP5 Lebensräume" },
         { name: "Bach/Fluss", group: "TOP5 Lebensräume" },
         { name: "Sonstiges", group: "TOP5 Lebensräume" }
       ]
@@ -291,7 +282,7 @@ export default {
   methods: {
     loadData() {
       this.tableData = [];
-      this.insects = [];
+      this.beobachtungen = [];
       this.loading = true;
       //fetch("/data/beobachtungen-" + this.selectedCampain.value + ".csv")
       fetch(
@@ -306,30 +297,30 @@ export default {
         .then(response => response.text())
         .then(data => {
           var results = Papa.parse(data, { header: true });
-          this.insects = results.data;
-          this.processData();
+          this.beobachtungen = results.data;
+          this.tableData=this.top100(this.beobachtungen);
         });
     },
-    processData() {
-      const unique = new Map();
-      this.insects.forEach(item => {
-        const entry = unique.get(item.artname);
+    top100(beobachtungen) {
+      const uniqueMap = new Map();
+      beobachtungen.forEach(beobachtung => {
+        const entry = uniqueMap.get(beobachtung.artname);
         if (!entry) {
-          unique.set(item.artname, {
-            artname: item.artname,
-            taxon: item.taxon,
-            gattung: item.gattung,
-            anzahl: Number.parseInt(item.anzahl),
-            meldungen: 1
+          uniqueMap.set(beobachtung.artname, {
+            artname: beobachtung.artname,
+            taxon: beobachtung.taxon,
+            gattung: beobachtung.gattung,
+            anzahl: Number.parseInt(beobachtung.anzahl),
+            meldungen: 0
           });
         } else {
-          ++entry.meldungen;
-          entry.anzahl += Number.parseInt(item.anzahl);
+          entry.meldungen++;
+          entry.anzahl += Number.parseInt(beobachtung.anzahl);
         }
       });
-      const top = [...unique.values()];
+      const top = [...uniqueMap.values()];
       top.sort(function(a, b) {
-        return b.meldungen - a.meldungen;
+        return b.meldungen - a.meldungen || b.anzahl - a.anzahl;
       });
 
       const top100 = top.slice(0, 100);
@@ -340,13 +331,28 @@ export default {
             Number.parseInt(top100[i].meldungen)
         ).toFixed(2);
       }
-      this.tableData = top100;
-      this.top100 = top100;
       this.footer = {
-        beobachtungen: this.insects.length ,
-        meldungen: anzahlMeldungen(this.insects)
+        beobachtungen: this.beobachtungen.length,
+        meldungen: anzahlMeldungen(this.beobachtungen)
       };
       this.loading = false;
+      return top100;
+    },
+    lebensraum(beobachtungen, lebensraum) {
+      const beobachtungenLebensraum = beobachtungen.filter(
+        beobachtung => beobachtung.lebensraum === lebensraum
+      );
+      this.footer.beobachtungen = beobachtungenLebensraum.length;
+      this.footer.meldungen = anzahlMeldungen(beobachtungenLebensraum);
+      return beobachtungenLebensraum;
+    },
+    bundesland(beobachtungen, bundesland) {
+      const beobachtungenBundesland = beobachtungen.filter(
+        beobachtung => beobachtung.bundesland === bundesland
+      );
+      this.footer.beobachtungen = beobachtungenBundesland.length;
+      this.footer.meldungen = anzahlMeldungen(beobachtungenBundesland);
+      return beobachtungenBundesland;
     },
     openTChart: function(props) {
       // TODO: Fix if other rankings are implemented!
@@ -361,6 +367,8 @@ export default {
         this.$set(this.$refs.dTable.expanded, item.artname, false);
       }
       this.tableData = [];
+      this.footer.meldungen= 0;
+      this.footer.beobachtungen= 0;
       this.selectedRanking = { name: "Top 100" };
       this.selectedCampain = this.campains.find(o => o.value == value);
       this.loadData();
@@ -368,28 +376,27 @@ export default {
     changeRanking: function(obj) {
       this.tableData = [];
       if (obj.name === "Top 100") {
-        this.tableData = this.top100;
-        //this.footer = {
-        //  meldungen: 2760,
-        //  beobachtungen: 23739
-        //};
-        // TODO: Fix reporting of 23740 entries(beobachtungen). Should be 23739. Because 1st must be the header. Strange
-        this.footer = {
-          beobachtungen: this.insects.length - 1,
-          meldungen: anzahlMeldungen(this.insects) - 1
-        };
-      } else if ( obj.group ==="TOP5 Lebensräume")
-      {
+        this.tableData = this.top100(this.beobachtungen);
+      } else if (obj.group === "TOP5 Lebensräume") {
         if (obj.name === "Garten") {
-          this.tableData = lebensraumTop5(this.insects, obj.name);
-          this.footer = anzahlLebensraum(this.insects, obj.name);
+          let beobachtungenLebensraum = this.lebensraum(
+            this.beobachtungen,
+            obj.name
+          );
+          this.tableData = top5(beobachtungenLebensraum);
         } else {
-          this.tableData = lebensraumTop5(this.insects, " " + obj.name);
-          this.footer = anzahlLebensraum(this.insects, " " + obj.name);
+          let beobachtungenLebensraum = this.lebensraum(
+            this.beobachtungen,
+            " " + obj.name
+          );
+          this.tableData = top5(beobachtungenLebensraum);
         }
-      } else if (obj.group === "TOP5 Bundesländer"){
-        this.tableData = top5bundesland(this.insects, obj.name);
-        this.footer = anzahlBundesland(this.insects, obj.name);
+      } else if (obj.group === "TOP5 Bundesländer") {
+        let beobachtungenBundesland = this.bundesland(
+          this.beobachtungen,
+          obj.name
+        );
+        this.tableData = top5(beobachtungenBundesland);
       }
       //close all expanded slots
       for (let i = 0; i < this.tableData.length; i += 1) {
