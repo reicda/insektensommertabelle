e3636294513e4933d1b1c9f0490fd48894aa5329
Bjoern Schilberg <bjoern.schilberg@intevation.de>
2020-05-25 00:36:24 +0200

    Fixed chart creation for lebensraeume

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -18,16 +18,14 @@ import juni2018_top5Bundeslaender from "./juni2018_top5Bundeslaender";
 import august2018_top100 from "./august2018_top100.js";
 import august2018_top5Lebensraeume from "./august2018_top5Lebensraeume";
 import august2018_top5Bundeslaender from "./august2018_top5Bundeslaender";
-import august2020_top100 from "./august2020_top100.js";
+import juni2019_top5Lebensraeume from "./juni2019_top5Lebensraeume";
+import august2019_top5Lebensraeume from "./august2019_top5Lebensraeume";
 
 export default {
   data: () => ({
     juni2018_top100: juni2018_top100,
-    juni2018_top5Lebensraeume: juni2018_top5Lebensraeume,
     juni2018_top5Bundeslaender: juni2018_top5Bundeslaender,
     august2018_top100: august2018_top100,
-    august2020_top100: august2020_top100,
-    august2018_top5Lebensraeume: august2018_top5Lebensraeume,
     august2018_top5Bundeslaender: august2018_top5Bundeslaender,
     selectedCampain: "",
     selectedRanking: "",
@@ -47,11 +45,10 @@ export default {
       aktion: { text: "August 2018", value: "2018-08" },
       top100: this.august2018_top100
     });
-    this.past_top100.push({
-      aktion: { text: "August 2020", value: "2020-08" },
-      top100: this.august2020_top100
-    });
-    console.log(this.past_top100);
+    this.past_top5Lebensraeume.push(juni2018_top5Lebensraeume)
+    this.past_top5Lebensraeume.push(august2018_top5Lebensraeume)
+    this.past_top5Lebensraeume.push(juni2019_top5Lebensraeume)
+    this.past_top5Lebensraeume.push(august2019_top5Lebensraeume)
   },
   mounted() {
     this.selectedCampain = this.$attrs["selected-campain"];
@@ -75,20 +72,19 @@ export default {
       this.tchartData.push(insect);
 
       const group = this.selectedRanking.value;
-      console.log(group);
 
       var month_index = this.selectedCampain.value.slice(-2);
       let tcd = this.tchartData;
 
       switch (group) {
         case "top100": {
-          this.prepareData(this.past_top100, group, insect, tcd, month_index);
+          this.prepareData(this.past_top100, this.selectedRanking, insect, tcd, month_index);
           break;
         }
         case "top5Bundeslaender":
           this.prepareData(
-            this.past_top5_bundeslaender,
-            group,
+            this.past_top5Bundeslaender,
+            this.selectedRanking,
             insect,
             tcd,
             month_index
@@ -96,8 +92,8 @@ export default {
           break;
         case "top5Lebensraeume":
           this.prepareData(
-            this.past_top5_lebensraeume,
-            group,
+            this.past_top5Lebensraeume,
+            this.selectedRanking,
             insect,
             tcd,
             month_index
@@ -107,16 +103,34 @@ export default {
           console.log("default");
       }
     },
-    prepareData: function(input, group, insect, tcd, month_index) {
-      console.log(group);
+    prepareData: function(input, ranking, insect, tcd, month_index) {
+
+      let group = ranking.value
+      let groupName = ranking.name
+
       let filteredItems = input.filter(
         o => o.aktion.value.slice(-2) === month_index
       );
+
+      if (group === "top100"){
       filteredItems.forEach(function(o) {
-        let found = o[group].find(o => o.name === insect.name);
+        let found = o[group].find(o => o.name=== insect.name);
         found.aktion = o.aktion.text;
         tcd.push(found);
+
       });
+      }else{
+      filteredItems.forEach(function(o) {
+        let found = o[group].find(o => o.name=== groupName);
+        let found2 = found["data"].find(x=> x.artname === insect.artname)
+        if (found2 !== undefined){
+
+        found2.aktion = o.aktion.text;
+        tcd.push(found2);
+        }
+        });
+
+      }
     },
     renderTChart: function() {
       var chart = new tauCharts.Chart({
