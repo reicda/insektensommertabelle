d762bcb3d38ef827d2e99c37a77df908c17dd70b
rrenkert <raimund@renkert.org>
2021-06-04 14:50:37 +0200

    Added campain 2021-06

--- src/components/TheTableHeader.vue
+++ src/components/TheTableHeader.vue
@@ -61,6 +61,7 @@ export default {
     return {
       search: "",
       campains: [
+        { text: "Juni 2021", value: "2021-06" },
         { text: "August 2020", value: "2020-08" },
         { text: "Juni 2020", value: "2020-06" },
         { text: "August 2019", value: "2019-08" },
